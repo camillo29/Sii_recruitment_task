3ff84fe0c3cc571a4c2c9112a22def222571d7e8
camillo29 <kamils33@interia.pl>
2022-05-20 11:44:00 +0200

    Initial commit

---
+++ src/main/java/com/sii/sii_recruitment_task/SiiRecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sii.sii_recruitment_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiRecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiRecruitmentTaskApplication.class, args);
+	}
+
+}
